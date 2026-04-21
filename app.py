a90b0081b6170e7367f91b27dd4644bf090a138f
joimar755 <joimarjose19@gmail.com>
2026-04-21 15:27:40 -0500

    detect

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 from fastapi import FastAPI, Depends, File, HTTPException, UploadFile
 from pydantic import BaseModel
-from models import Cultivos, Estaciones, LecturaSensores, RecomendacionesRag
+from models import Cultivos, Estaciones, LecturaSensores, RecomendacionesRag, Detecciones
 import models
 from database import engine , SessionLocal
 from sqlalchemy.orm import Session
@@ -123,26 +123,43 @@ async def create_cultivo(data: Ph, db: Session = Depends(get_db)):
         
         return {"data": db_lectura}
     
+
 @app.post("/detect/")
-async def detect_objects(file: UploadFile = File(...)):
-    # Read image file
+async def detect_objects(
+    file: UploadFile = File(...),
+    db: Session = Depends(get_db)
+):
+    cultivo = db.query(Cultivos).order_by(Cultivos.id.desc()).first()
+
+    if not cultivo:
+        return {"error": "No hay cultivos registrados"}
+
+    cultivo_id = cultivo.id
+
     image_bytes = await file.read()
     nparr = np.frombuffer(image_bytes, np.uint8)
     image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    
-    # Run YOLO model
+
     results = yolo_model(image)
-    detections = []
-    
+
+    detecciones = []
+
     for result in results:
         for box in result.boxes:
-            x1, y1, x2, y2 = map(int, box.xyxy[0])
             class_id = int(box.cls[0])
-            confidence = float(box.conf[0])
-            detections.append({
-                "label": yolo_model.names[class_id],
-                "confidence": confidence,
-                "bbox": [x1, y1, x2, y2]
-            })
-    
-    return JSONResponse(content={"detections": detections})
+            label = yolo_model.names[class_id]
+
+            nueva_det = Detecciones(
+                nombre=label,
+                cultivo_id=cultivo_id
+            )
+
+            db.add(nueva_det)
+            detecciones.append(label)
+
+    db.commit()
+
+    return {
+        "cultivo_id": cultivo_id,
+        "detecciones": detecciones
+    }
